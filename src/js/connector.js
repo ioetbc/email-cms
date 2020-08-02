2f206dc6c18fd631196d3b58fe238bedc7fd11b5
William Cole <ioetbc@gmail.com>
2020-08-02 14:38:01 +0100

    btb

--- src/js/connector.js
+++ src/js/connector.js
@@ -6,39 +6,7 @@ window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
       return t.card('all')
       .then(async data => {
-          console.log('data', data)
-
-
-          // 1. if template doesnt exist create the templates as card in trello
-          const octokit = new Octokit({ auth: '%%GITHUB_AUTH%%' });
-          const fileContent = await octokit.request('GET /repos/ioetbc/email-trello-cms/contents/email.html', {
-            owner: 'ioetbc',
-            repo: 'email-trello-cms',
-            path: 'email.html',
-            type: 'public'
-          });
-          console.log('fileContent', fileContent)
-        //   const emailHtml = Base64.decode(fileContent.data.content)
-        //   // convert html to markdown
-        //   const turndownService = new window.TurndownService();
-        //   const emailMarkdown = turndownService.turndown(emailHtml);
-        //   // console.log('emailMarkdown', emailMarkdown)
-        //   const JustTheEmailcontent = emailMarkdown.split('Hi there,')[1];
-
-        //   console.log('just the content', JustTheEmailcontent);
-
-          
-
-          // 2. create a pull request with the data from the card
-
-
-
-
-
-
-
-
-
+          console.log('data')
           return [{
               text: card.idShort
           }]
