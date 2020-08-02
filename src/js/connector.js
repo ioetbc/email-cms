1dca156108873df770682bae5d869894c4340890
William Cole <ioetbc@gmail.com>
2020-08-02 14:57:27 +0100

    btb

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,12 +1,11 @@
 import { Octokit } from "@octokit/core";
 import { Base64 } from 'js-base64';
 import Trello from 'trello';
-
+console.log('data')
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
       return t.card('all')
       .then(async data => {
-          console.log('data')
           return [{
               text: card.idShort
           }]
